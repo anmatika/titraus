9a05cffd4a85714ff06260370c3240e27b1ce3fe
Antti Matikainen <4ntti.matikainen@gmail.com>
2018-02-04 14:13:55 +0200

    add flow

--- outputter.js
+++ outputter.js
@@ -1,16 +1,9 @@
-// @flow
 const chalk = require('chalk');
 
-type Amounts = {
-  mlBottle: number,
-  mgPills: number,
-  mgTarget: number,
-};
-
-function calculate({ mlBottle, mgPills, mgTarget }: Amounts) {
+function calculate({ mlBottle, mgPills, mgTarget }) {
   return mlBottle / mgPills * mgTarget;
 }
-function print({ mlBottle, mgPills, mgTarget }: Amounts) {
+function print({ mlBottle, mgPills, mgTarget }) {
   const mlToDrink = calculate({ mlBottle, mgPills, mgTarget });
 
   console.log(`${chalk.gray('Drink')} ${chalk.green(mlToDrink)} ml to get ${mgTarget} mg of med`);
